6f53aeb9e499a2db3ec66027000a1d8dd2f16d2e
xyz-pj <89639494+xyz-pj@users.noreply.github.com>
2024-10-21 16:34:58 +0900

    Add files via upload

--- StoneAge/scripts.js
+++ StoneAge/scripts.js
@@ -93,10 +93,9 @@ function calculateStats() {
     const petDef = parseInt(document.getElementById('petDef').value) || 0;
     const petAgi = parseInt(document.getElementById('petAgi').value) || 0;
 
-    const finalRidingatk = (offensive * 0.7) + (petAtk * 0.7) + itemAtk;
-    const finalRidingdef = (defensive * 0.7) + (petDef * 0.7) + itemDef;
-    const finalRidingagi = (speed * 0.2) + (petAgi * 0.8) + itemAgi;
-
+    const finalRidingatk = Math.floor((offensive * 0.7) + (petAtk * 0.7) + itemAtk);
+    const finalRidingdef = Math.floor((defensive * 0.7) + (petDef * 0.7) + itemDef);
+    const finalRidingagi = Math.floor((speed * 0.2) + (petAgi * 0.8) + itemAgi);
 
     document.getElementById('finalRidingatk').innerText = finalRidingatk;
     document.getElementById('finalRidingdef').innerText = finalRidingdef;
